9eda68661df92c13c249db3288162e690c86a4c5
Lumière Élevé <88174309+PoneyClairDeLune@users.noreply.github.com>
2022-04-20 13:58:35 +0000

    Installation script for WGCF

---
+++ wgcf.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+appVer="2.2.12"
+echo "Detecting device architecture..."
+debArch=$(uname -m)
+transArch=$debArch
+case $debArch in
+	"i386" | "i686")
+		transArch=386
+		;;
+	"x86_64" | "amd64")
+		transArch=amd64
+		;;
+	"arm64" | "armv8l" | "aarch64")
+		transArch="arm64"
+		;;
+esac
+echo "Installing WGCF on your device..."
+mkdir -p $PREFIX/opt/wgcf
+cd $PREFIX/opt/wgcf
+curl -Lo wgcf "https://github.com/ViRb3/wgcf/releases/download/v${appVer}/wgcf_${appVer}_linux_${transArch}" && chmod +x wgcf
+ln -s $PREFIX/opt/wgcf/wgcf /bin/wgcf
+echo "Installation complete."
+exit
