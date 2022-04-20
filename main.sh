0c5c79c4a78fa36e3c41a4f4d044128a29f59a33
Lumière Élevé <88174309+PoneyClairDeLune@users.noreply.github.com>
2022-04-20 14:35:58 +0000

    "Compilation" source

--- main.sh
+++ main.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 use ./wgcf.sh
 use ./wireproxy.sh
-use ./registerService.sh
-use ./newIdentity.sh
 echo "Updating helper script..."
 mkdir -p $PREFIX/opt/nightglow
 cd $PREFIX/opt/nightglow
 curl -Lo nightglow.sh https://github.com/ltgcgo/nightglow/raw/main/nightglow.sh
 chmod +x nightglow.sh
 ln -s $PREFIX/opt/nightglow/nightglow.sh $PREFIX/bin/nightglow
+useradd nightglow -m -s /usr/bin/bash
+echo "Registering a new identity..."
+nightglow init
+nightglow useSocks 20040
 echo "Nightglow is now on your device. If you are not using WARP+, you can now safely remove the official WARP client on your device."
 exit
